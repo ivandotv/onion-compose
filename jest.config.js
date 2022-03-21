5b0bbba4bd183e3375ba73b20ac6ec29d73bdbb9
ivandotv <390700+ivandotv@users.noreply.github.com>
2022-03-21 22:38:35 +0100

    fix jest coverage

--- jest.config.js
+++ jest.config.js
@@ -5,22 +5,12 @@ module.exports = {
   ],
   testEnvironment: 'node',
   moduleDirectories: ['node_modules', 'src'],
-  // moduleNameMapper: {
-  //   '~/(.*)': '<rootDir>/src/$1'
-  // },
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname'
   ],
-  globals: {
-    __DEV__: false, //toggle to true to execute the code in __DEV__ scope
-    __VERSION__: 'jest-version',
-    __BUILD_DATE__: 'jest-build-date',
-    __COMMIT_SHA__: 'jest-commit-sha'
-  },
   collectCoverageFrom: [
     '<rootDir>/src/**',
-    '!<rootDir>/src/index.ts',
     '!<rootDir>/src/globals.d.ts',
     '!<rootDir>/src/__tests__/**',
     '!<rootDir>/src/__fixtures__/**'
